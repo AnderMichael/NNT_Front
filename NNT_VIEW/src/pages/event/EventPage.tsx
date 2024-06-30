a55335f8b58d4d471418628e81298efe2db4a062
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-06-30 00:29:38 -0400

    feat: Added HomePage and EventPage

--- NNT_VIEW/src/pages/event/EventPage.tsx
+++ NNT_VIEW/src/pages/event/EventPage.tsx
@@ -1,7 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import img from "../../assets/BsArrowLeftCircleFill.png";
+import img2 from "../../assets/image 47.png";
+import mapa from "../../assets/mapa.png";
+
 const EventPage = () => {
+
+    const navigate = useNavigate();
+
     return(
         <>
-            <div > Aqui esta el eventitouwu </div>
+            <div className="fixed m-2">
+                <button onClick={() => navigate("/home")}>
+                <img src={img} alt="Go Back" />
+                </button>
+            </div>
+            <div className="flex flex-col">
+                <img src={img2} alt="" className="px-10 pt-5 rounded-lg" />
+                <div className="m-auto text-xl">
+                    <h3><strong>Nombre del evento:</strong> Fiestita</h3>
+                    <p><strong>Fecha:</strong> 20-10-2024</p>
+                    <p><strong>Hora:</strong> 13:00</p>
+                    <p><strong>Lugar:</strong> La Paz</p>
+                    <p><strong>Costo:</strong> 25</p>
+                </div>
+            </div>  
+            <div className="mb-28 mt-5 flex justify-center">
+                <img src={mapa} alt="" />
+            </div>
+            
+            <div className="fixed bottom-10 right-10">
+                <button className="bg-yellow-300 text-2xl font-bold p-2 w-80 rounded-full shadow-xl">Participare</button>
+            </div>
+            
         </>
     );
 }
